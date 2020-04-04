0d986f1e3b3ad4a3b488323a0a808a9f68c5048b
sangpham2710 <sangpham2710@users.noreply.github.com>
2020-04-04 12:51:37 +0700

    Update fractal_tree.py

--- fractal_tree.py
+++ fractal_tree.py
@@ -1,28 +1,29 @@
 from turtle import *
-angle = 35
+angle = 90
 t = Turtle()
-t.speed(0)
-t.hideturtle()
-
-def f(l):
-	ox = t.xcor()
-	oy = t.ycor()
-	if (l > 1):
-		t.rt(angle)
-		t.fd(l)
-		f(l*0.66)
-		t.goto(ox,oy)
-		t.lt(angle)
-
-		t.lt(angle)
-		t.fd(l)
-		f(l*0.66)
-		t.goto(ox,oy)
-		t.rt(angle)
-
-t.home()
-t.clear()
-t.lt(90)
-t.bk(150)
-t.fd(150)
-f(100)
+def setup():
+    t.speed(0)
+    t.hideturtle()
+    t.getscreen().delay(0)
+    t.getscreen().tracer(0)
+    t.home()
+    t.clear()
+    t.lt(90)
+    t.bk(150)
+    t.fd(150)
+def draw(l):
+    ox = t.xcor()
+    oy = t.ycor()
+    if (l > 1):
+        t.rt(angle)
+        t.fd(l)
+        draw(l * 2 / 3)
+        t.goto(ox, oy)
+        t.lt(angle)                                    
+        t.lt(angle)
+        t.fd(l)                                   
+        draw(l * 2 / 3)
+        t.goto(ox, oy)
+        t.rt(angle)
+setup()
+draw(100)
